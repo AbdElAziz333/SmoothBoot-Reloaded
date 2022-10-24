a32e14dfec470f5af2f2ac643e26ce736bec2460
AbdElAziz333 <89092953+AbdElAziz333@users.noreply.github.com>
2022-10-24 22:45:46 +0200

    fix: update method parameters

--- src/main/java/com/abdelaziz/smoothboot/mixin/client/IntegratedServerMixin.java
+++ src/main/java/com/abdelaziz/smoothboot/mixin/client/IntegratedServerMixin.java
@@ -2,12 +2,14 @@ package com.abdelaziz.smoothboot.mixin.client;
 
 import com.abdelaziz.smoothboot.SmoothBoot;
 import com.abdelaziz.smoothboot.config.SmoothBootConfigHandler;
+import com.mojang.authlib.GameProfileRepository;
+import com.mojang.authlib.minecraft.MinecraftSessionService;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.server.IntegratedServer;
-import net.minecraft.server.Services;
 import net.minecraft.server.WorldStem;
 import net.minecraft.server.level.progress.ChunkProgressListenerFactory;
 import net.minecraft.server.packs.repository.PackRepository;
+import net.minecraft.server.players.GameProfileCache;
 import net.minecraft.world.level.storage.LevelStorageSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,7 +20,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class IntegratedServerMixin {
     @Inject(method = "<init>", at = @At("RETURN"))
     public void onInit(Thread p_235248_, Minecraft p_235249_, LevelStorageSource.LevelStorageAccess p_235250_,
-                       PackRepository p_235251_, WorldStem p_235252_, Services p_235253_,
+                       PackRepository p_235251_, WorldStem p_235252_, MinecraftSessionService p_235253_,
+                       GameProfileRepository p_205659_, GameProfileCache p_205660_,
                        ChunkProgressListenerFactory p_235254_, CallbackInfo ci) {
         p_235248_.setPriority(SmoothBootConfigHandler.config.getIntegratedServerPriority());
         SmoothBoot.LOGGER.debug("Initialized integrated server thread");
